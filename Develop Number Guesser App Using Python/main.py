6430e76bfa29b0b31a50f6add4c1c3312c70668b
ibrahimohid <ibrahimohid77@gmail.com>
2023-11-30 19:06:17 +0600

    Develop Number Guesser App Using Python updated

--- Develop Number Guesser App Using Python/main.py
+++ Develop Number Guesser App Using Python/main.py
@@ -1,19 +1,21 @@
-from random import randint;
+
+import random
 
 for x in range(1, 6):
     low = 1;
     high = 50;
 
-    guessNumber = int(input("Enter a Number 1 to 50: "));
-    correct_answer = randint(low, high);
-    if guessNumber < correct_answer:
+    correct_answer = random.randrange(low, high);
+    user_input = int(input("Enter a Number from 1 to 50: "));
+
+    if correct_answer > user_input:
         print("Correct answer is greater!");
-        print("Guess Number Was", correct_answer)
-    elif guessNumber > correct_answer:
-        print("Correct answer is smaller!")
-        print("Guess Number Was", correct_answer)
-    elif guessNumber == correct_answer:
-        print("You Win");
-        print("Guess Number Was", correct_answer)
-    else:
-        print("You Lose!")
+        print("Guess Number Was", correct_answer);
+    elif correct_answer < user_input:
+        print("Correct answer is smaller!");
+        print("Guess Number Was", correct_answer);
+    elif correct_answer == user_input:
+        print(" You Win");
+        print("Guess Number Was", correct_answer);
+        break;
+print("You Lose");
