08f2571f20cb5ac98c68b4ab12cd8797e796e620
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-06 21:11:24 +0600

    Develop Number Guesser App Using Python updated

--- Develop Number Guesser App Using Python/main.py
+++ Develop Number Guesser App Using Python/main.py
@@ -1,21 +1,49 @@
+#
+# import random
+#
+# low = 1;
+# high = 50;
+#
+# correct_answer = random.randrange(low, high);
+# user_input = int(input("Enter a Number from 1 to 50: "));
+# while user_input > 5:
+#     if correct_answer > user_input:
+#         print("Correct answer is greater!");
+#         print("Guess Number Was", correct_answer);
+#     elif correct_answer < user_input:
+#         print("Correct answer is smaller!");
+#         print("Guess Number Was", correct_answer);
+#     elif correct_answer == user_input:
+#         print(" You Win");
+#         print("Guess Number Was", correct_answer);
+#         break;
+#     print("You Lose!")
 
-import random
+from random import randint
 
-for x in range(1, 6):
-    low = 1;
-    high = 50;
+low = 1
+high = 50
 
-    correct_answer = random.randrange(low, high);
-    user_input = int(input("Enter a Number from 1 to 50: "));
 
-    if correct_answer > user_input:
-        print("Correct answer is greater!");
-        print("Guess Number Was", correct_answer);
-    elif correct_answer < user_input:
-        print("Correct answer is smaller!");
-        print("Guess Number Was", correct_answer);
-    elif correct_answer == user_input:
-        print(" You Win");
-        print("Guess Number Was", correct_answer);
-        break;
-print("You Lose");
+def play():
+    limit = 5
+    correct_answer = randint(low, high)
+
+    while limit > 0:
+        try:
+            user_input = int(input("Input a number 1 to 50: "))
+        except ValueError as e:
+            print("Please Input a Number instead of String.")
+            play()
+        if user_input < correct_answer:
+            print("Correct answer is greater!")
+        elif user_input > correct_answer:
+            print("Correct answer is smaller!")
+        elif user_input == correct_answer:
+            print("You win!")
+        limit -= 1
+    print("You Lose!")
+
+
+if __name__ == "__main__":
+    play()
